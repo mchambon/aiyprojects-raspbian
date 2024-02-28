950314c4a5e081bb3664260c7a564d104eceaab3
mchambon <mat.chambon@gmail.com>
2024-02-28 13:38:05 +0100

    Update setup.py
    remove google cloud requirements

--- setup.py
+++ setup.py
@@ -16,11 +16,6 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
-        'google-assistant-library>=1.0.1',
-        'google-assistant-grpc>=0.2.0',
-        'google-auth>=1.5.1',
-        'google-auth-oauthlib>=0.2.0',
-        'google-cloud-speech>=0.36.0',
         'gpiozero',
         'protobuf>=3.6.1',
         'picamera',
